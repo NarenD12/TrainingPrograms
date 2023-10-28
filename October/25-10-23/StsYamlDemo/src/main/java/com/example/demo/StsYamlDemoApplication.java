f24080af9a875314adbc28237ce7825a661d30db
NarenD12 <144924131+NarenD12@users.noreply.github.com>
2023-10-28 23:37:13 +0530

    Spring boot Yaml demo

---
+++ October/25-10-23/StsYamlDemo/src/main/java/com/example/demo/StsYamlDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StsYamlDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StsYamlDemoApplication.class, args);
+	}
+
+}
